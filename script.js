90c18967d17ad84d899209fb4ad1d83e2f79536f
Masud Sarder <msmd270@gmail.com>
2022-02-17 19:50:49 +0600

    second strap finis

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-function westMoneyAll(billid){
-    const foodBillInput = document.getElementById(billid + '-money');
-    const foodBill = parseFloat(foodBillInput.value);
-    foodBillInput.value = '';
-    return foodBill;
+// function for food rent clothes
+function westMoneyAll(billid){ 
+        const montleyBillInput = document.getElementById(billid + '-money');
+        const montleyBill = parseFloat(montleyBillInput.value);
+        if(montleyBill > 0){
+            montleyBillInput.value = '';
+            return montleyBill;
+        } else {
+            alert('Opps your type is worng please try again')
+        }
+        
 }
-
-
-document.getElementById('calcule-btn').addEventListener('click', function(){
+function totalIncome(){
     const moneySelaryInput = document.getElementById('montley-salary');
     const montleySelary = parseFloat(moneySelaryInput.value);
+    if(montleySelary > 0){
+        moneySelaryInput.value = '';
+    return montleySelary;
+    } else {
+        alert('Opps Your Income Is Worng Please HardWork Agin I Hope You Can do This Insahallah')
+    }
+}
+
+document.getElementById('calcule-btn').addEventListener('click', function(){
+    // use the function top 
+    const montleySelary = totalIncome();
     const foodBill = westMoneyAll('food');
     const romRent = westMoneyAll('rom');
     const clothesBill = westMoneyAll('clother');
+
     const totalWestBill = foodBill + romRent + clothesBill;
+    // show the this main work 
     const westMoneyInput = document.getElementById('west-money');
     const extayMoney = montleySelary - totalWestBill;
     const showExtrayInput = document.getElementById('extra-money');
     westMoneyInput.innerText = totalWestBill;
     showExtrayInput.innerText = extayMoney;
-    moneySelaryInput.value = '';
-
-})
+});
+// document.getElementById('saving-btn').addEventListener('click', function(){
+//     const savingInput = document.getElementById('saving-input');
+//     const saving = parseFloat(savingInput.value);
+//     const 
+//     const totalAmount = totalIncome();
+//     const savingTotalAmount =totalAmount / 10;
+    
+// })
 
 
